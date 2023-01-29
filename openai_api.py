acb30f7004ece2ec7085e07492938dcbc78e0cab
sahaavi <avisheksaha123@gmail.com>
2023-01-28 18:26:08 -0800

    Make api functions

---
+++ openai_api.py
@@ -0,0 +1,60 @@
+import openai
+
+def get_skills(response):
+    """
+    Extract only the skills with percentage lines
+    from api's response.
+    """
+    text_list = [choice.text for choice in response['choices']]
+    return text_list
+
+def skills_percentage(response):
+    """
+    Make a dictionary of skills with percentages. 
+    """
+    skills_percent = {"Skills": [], "Percentage": []}
+    for text in response:
+        text = text.replace('\n', '')
+        for line in text.split('%'):
+            if line == '':
+                pass
+            else:
+                skill, percentage = line.split(': ')
+                skills_percent["Skills"].append(skill)
+                skills_percent["Percentage"].append(percentage)
+    return skills_percent
+
+def call_api(api_key, job_description):
+    openai.api_key = api_key
+    pre_prompt = """Based on the job description identify the required skills in percentage of 100. 
+                Job Description:
+                """
+    post_prompt = """
+                Give the answer like this-
+                Skill: Percentage:"""
+    prompt = pre_prompt + job_description + post_prompt
+
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            temperature=0.1,
+            prompt=prompt,
+            max_tokens=100)
+        response = get_skills(response)
+        result = skills_percentage(response)
+        return result
+
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        print(f"OpenAI API returned an API Error: {e}")
+        pass
+
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        print(f"Failed to connect to OpenAI API: {e}")
+        pass
+
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        print(f"OpenAI API request exceeded rate limit: {e}")
+        pass
