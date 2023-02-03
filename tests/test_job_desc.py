1265781a6eefc5ca590a869985d2625b8f58b83f
Viji <vijina@student.ubc.ca>
2023-02-02 20:43:19 -0800

    initialtest

---
+++ tests/test_job_desc.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+sys.path.append("e:\\Study\\UBC\\Block 4\\DATA 534 Web and Cloud Computing\\Project\\Job-to-Skill")
+sys.path.append("C:\\Users\\vijip\\DATA\DATA534\\Job-to-Skill")
+import os
+
+import unittest
+from dotenv import load_dotenv
+
+from job_skill import job_desc.py as jd
+
+class TestJobDescScrp(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        load_dotenv()
+        print("job_Skill test starts")
+
+    # setting up for test
+    def setUp(self):
+        print("Test Setup")
+
+    # test end
+    def tearDown(self):
+        print("Test End")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("job_Skill test finishes")
+
+    # test case
+#    def test_call_api_skills_percent(self): 
+#        self.assertIsInstance(oa.call_api_skills_percent(self.api_key, self.job_description), dict)
+
+    # test case
+#    def test_call_api_tech_skills(self): 
+#        self.assertIsInstance(oa.call_api_tech_skills(self.api_key, self.job_description), dict)
+
+unittest.main(argv=[''], verbosity=2, exit=False)
+        
+
--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,11 +1,13 @@
 import unittest
 
 from test_openai_api import TestOpenaiApi
+from test_openai_api import TestJobDescScrp
 
 def my_suite():
     suite = unittest.TestSuite()
     # result = unittest.TestResult()
     suite.addTest(unittest.makeSuite(TestOpenaiApi))
+    suite.addTest(unittest.makeSuite(TestJobDescScrp))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 
