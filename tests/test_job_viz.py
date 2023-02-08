7b7f5831bd5e63587e23cf28f8e6729365d064d5
nom0786 <noman0786@hotmail.com>
2023-02-08 03:24:32 -0800

    test cases

---
+++ tests/test_job_viz.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('/Users/nomanmohammad/Desktop/Job-to-Skill')
+import os
+import pandas as pd
+import altair as alt
+
+import unittest
+
+from job_skill import job_viz as jv
+
+class TestJobVisualization(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        
+        print("job_viz test starts")
+
+        cls.df = pd.DataFrame({'Job URL': ['https://www.linkedin.com/jobs/view/3472248063'], 
+        'Tools': ['Azure/AWS, MLOps, Data Management and Analytics Platforms'],
+        'Programming Languages': ['Java, C#, Ruby, Python'],
+        'Job Location': 'Toronto, ON, Canada'})
+
+        cls.df_tools = pd.DataFrame({'Tools': ['AWS', 'Azure', 'Excel']})
+
+        cls.df_lang = pd.DataFrame({'Programming Languages': ['Java', 'C#', 'Ruby', 'Python']})
+
+    # setting up for test
+    def setUp(self):
+        print("Test Setup")
+
+    # test end
+    def tearDown(self):
+        print("Test End")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("job_viz test finishes")
+
+    # test case
+    def test_parse_df(self): 
+        self.assertIsInstance(jv.parse_df(self.df, 'Tools'), pd.DataFrame)
+
+    # test case
+    def test_visualize_info(self): 
+        self.assertIsInstance(jv.visualize_info(self.df_tools, self.df_lang), alt.Chart)
+
+    # test case
+    def test_visualize_location(self): 
+        self.assertIsInstance(jv.visualize_location(self.df, 'Job Location'), alt.Chart)
+
+unittest.main(argv=[''], verbosity=2, exit=False)
+
+import altair as alt
+
+
--- tests/test_openai_api.py
+++ tests/test_openai_api.py
@@ -1,9 +1,11 @@
 import sys
-sys.path.append("e:\\Study\\UBC\\Block 4\\DATA 534 Web and Cloud Computing\\Project\\Job-to-Skill")
+# sys.path.append("e:\\Study\\UBC\\Block 4\\DATA 534 Web and Cloud Computing\\Project\\Job-to-Skill")
+sys.path.append('/Users/nomanmohammad/Desktop/Job-to-Skill')
 import os
+import pandas as pd
 
 import unittest
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 from job_skill import openai_api as oa
 
@@ -11,9 +13,10 @@ class TestOpenaiApi(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        load_dotenv()
+        # load_dotenv()
         print("openai_api test starts")
-        cls.api_key = os.getenv("API_KEY")
+        # cls.api_key = os.getenv("API_KEY")
+        cls.api_key = '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'
         cls.job_description = """Understand the day-to-day issues that our business faces, which can be better understood with data
             Collecting and analyzing data from a variety of sources (such as college and university websites, government databases, and industry reports)
             Cleaning, formatting, and organizing the data in a way that is useful and easy to understand
@@ -23,6 +26,10 @@ class TestOpenaiApi(unittest.TestCase):
             Continuously monitoring and updating the data set to ensure its relevance and accuracy
             Compile and analyze data related to business' issues
             Develop clear visualizations to convey complicated data in a straightforward fashion."""
+
+        cls.job_skills = [['python', 'SQL', 'java']]
+        cls.interview_questions = ['What Big Data Technologies have you worked with?', 'What strategies have you used to capture and develop ideas?']
+
 
     # setting up for test
     def setUp(self):
@@ -44,5 +51,16 @@ class TestOpenaiApi(unittest.TestCase):
     def test_call_api_tech_skills(self): 
         self.assertIsInstance(oa.call_api_tech_skills(self.api_key, self.job_description), dict)
 
+    # test case
+    def test_call_api_questions(self): 
+        self.assertIsInstance(oa.call_api_questions(self.api_key, self.job_skills[0]), list)
+
+    # test case
+    def test_call_api_answers(self): 
+        self.assertIsInstance(oa.call_api_answers(self.api_key, self.interview_questions), list)
+
+    # test case
+    def test_call_api_interview(self): 
+        self.assertIsInstance(oa.call_api_interview(self.api_key, self.job_skills), pd.DataFrame)
+
 unittest.main(argv=[''], verbosity=2, exit=False)
-        
