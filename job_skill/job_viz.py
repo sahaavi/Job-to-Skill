3fc9837821fc00a2efa80ba09161cd92797edfac
nom0786 <noman0786@hotmail.com>
2023-02-08 02:19:02 -0800

    added doc strings for functions

--- job_skill/job_viz.py
+++ job_skill/job_viz.py
@@ -3,6 +3,32 @@ import altair as alt
 
 def parse_df(df, columnname):
 
+    """
+    Parse and clean data in preparation for visualization
+
+    Parameters
+    ----------
+    df : dataframe 
+        Data frame containing job information
+    columnname : str
+        The column name for which data parsing is needed
+        
+    Returns
+    -------
+    Data Frame
+        returns a new df containing only the column needed for visualization
+
+    Examples
+    --------
+    >>> parse_df(jobs_df, 'tools')
+
+        [tools] 
+        [1]  
+        [2]         
+        [...]
+        [n]
+
+    """
     try:
         column_list = df[columnname].tolist()
         list_full = []
@@ -19,9 +45,35 @@ def parse_df(df, columnname):
 
         return df
     except Exception as e:
-        return "Error in chart creation: " + str(e)
+        return "Error in data parsing: " + str(e)
+
 
 def visualize_info(df, df2):
+
+    """
+    Visualization for tools/languages from the jobs dataframe
+
+    Parameters
+    ----------
+    df : dataframe 
+        Data frame containing cleaned job information (programming languages)
+    df2 : dataframe
+       Second data frame containing cleaned job information (tools)
+        
+    Returns
+    -------
+    visualization
+        returns 2 stacked barcharts 
+
+    Examples
+    --------
+    >>> parse_df(df_tools, df_skills)
+
+      [barchart 1]
+
+      [barchart 2]
+
+    """
     try:
         chart = (
         alt.Chart(df).mark_bar(color = '#FFCE54').encode(
@@ -44,6 +96,30 @@ def visualize_info(df, df2):
         return "Error in chart creation: " + str(e)
 
 def visualize_location(df, columnname):
+
+    """
+    Visualization for distribution of job locations with data parsing
+
+    Parameters
+    ----------
+    df : dataframe 
+        Data frame containing unclean job information
+    columnname : str
+       The column name for which data parsing is needed
+        
+    Returns
+    -------
+    visualization
+        returns a piechart 
+
+    Examples
+    --------
+    >>> parse_df(df_jobs, 'job location')
+
+      [pie chart]
+
+    """
+
     if df[columnname].notna().all():
         try:
             temp = df[columnname].tolist()
@@ -72,7 +148,3 @@ def visualize_location(df, columnname):
     else:
         print("No locations available to build chart!")
         return False
-
-
-
-
